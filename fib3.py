55bf2957ca65ca0d999f3f662e588dccf81aa2a4
ruem <ruem@kswe.ch>
2024-08-14 14:51:34 +0200

    Corrected mistake with append

--- fib3.py
+++ fib3.py
@@ -5,7 +5,7 @@ def fib3(n):
     if n<len(results):
         return results[n]
     else:
-        results.append(n,fib3(n-2)+fib3(n-1))
+        results.append(fib3(n-2)+fib3(n-1))
         return results[n]
     
 print(fib3(10))
